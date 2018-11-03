cab883cb108520c02b0e306bb0c947d3e6251f6a
BEEMO\shurns <shburns02@gmail.com>
2018-11-03 19:19:32 -0400

    Dns Samples

---
+++ Chapter 2/Dns_Samples/DnsTest/DnsTestProgram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace DnsTest
+{
+    class DnsTestProgram {
+        static void Main(string[] args) {
+            var domainEntry = Dns.GetHostEntry("fun.with.dns.com");
+            Console.WriteLine(domainEntry.HostName);
+            foreach (var ip in domainEntry.AddressList) {
+                Console.WriteLine(ip);
+            }
+
+            var domainEntryByAddress = Dns.GetHostEntry("127.0.0.1");
+            Console.WriteLine(domainEntryByAddress.HostName);
+            foreach (var ip in domainEntryByAddress.AddressList) {
+                Console.WriteLine(ip);
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
---
+++ Chapter 2/Dns_Samples/UriTest/UriTestProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace UriTest
+{
+    public class UriTestProgram
+    {
+        public static Uri GetSimpleUri() {
+            var builder = new UriBuilder();
+            builder.Scheme = "http";
+            builder.Host = "packt.com";
+            return builder.Uri;
+        }
+
+        public static Uri GetSimpleUri_Constructor() {
+            var builder = new UriBuilder("http", "packt.com");
+            return builder.Uri;
+        }
+
+        public static void Main(string[] args)
+        {
+            var simpleUri = GetSimpleUri();
+            Console.WriteLine(simpleUri.ToString());
+            // Expected output: http://packt.com
+
+            var constructorUri = GetSimpleUri_Constructor();
+            Console.WriteLine(simpleUri.ToString());
+            // Expected output: http://packt.com
+
+            Thread.Sleep(10000);
+        }
+    }
+}
