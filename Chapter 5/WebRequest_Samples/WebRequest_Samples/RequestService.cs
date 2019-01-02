875610ecbc6897e4a8932ec7f130dc1dbfd35491
BEEMO\shurns <shburns02@gmail.com>
2019-01-01 22:22:04 -0500

    Initial samples

---
+++ Chapter 5/WebRequest_Samples/WebRequest_Samples/RequestService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Net.Security;
+
+namespace WebRequest_Samples
+{
+    public class RequestService
+    {
+        private static readonly string FINANCE_CONN_GROUP = "financial_connection";
+        private static readonly string REAL_ESTATE_CONN_GROUP = "real_estate_connection";
+
+        public static void Main(string[] argsa) {
+            SubmitRealEstateRequest();
+        }
+
+        public static void SubmitRealEstateRequest() 
+        {
+            WebRequest req = WebRequest.Create("https://real-estate-detail.com/market/api");
+            req.ConnectionGroupName = REAL_ESTATE_CONN_GROUP;
+            var noCachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
+            req.CachePolicy = noCachePolicy;
+            req.AuthenticationLevel = AuthenticationLevel.MutualAuthRequired;
+            req.Credentials = new NetworkCredential("test_user", "secure_and_safe_password");
+            Stream reqStream = req.GetRequestStream();
+            var messageString = "test";
+            var messageBytes = Encoding.UTF8.GetBytes(messageString);
+            reqStream.Write(messageBytes, 0, messageBytes.Length);
+        }
+    }
+}
