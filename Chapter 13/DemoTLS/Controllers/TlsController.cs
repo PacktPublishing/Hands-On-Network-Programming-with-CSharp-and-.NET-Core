53a2c92ce08554f7a7b77c27ccc47aada9b7869c
BEEMO\shurns <shburns02@gmail.com>
2019-02-22 03:23:55 -0500

    Chapter 13 demo app

---
+++ Chapter 13/DemoTLS/Controllers/TlsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DemoTLS {
+    [Route("demo/[controller]")]
+    [ApiController]
+    public class TlsController : ControllerBase {
+        [HttpGet("initiate-connection")]
+        public ActionResult<string> GetCertificate() {
+            return "CERTIFICATE_DELIVERED";
+        }
+
+        [HttpGet("certificate-verified")]
+        public ActionResult<string> GetVerification() {
+            return "PUBLIC_KEY_FOR_ENCRYPTING_HANDSHAKE";
+        }
+
+        [HttpGet("hash-algorithms-requested")]
+        public ActionResult<IEnumerable<string>> GetAlgorithms() {
+            return new string[] {
+                "SHA-256",
+                "AES",
+                "RSA"
+            };
+        }
+
+        [HttpPost("hash-algorithm-selected")]
+        public ActionResult<string> Post([FromBody] string sharedAlgorithm) {
+            SessionService.CurrentAlgorithm = sharedAlgorithm;
+            return SessionService.GenerateSharedKeyWithPrivateKeyAndRandomSessionKey();
+        }
+    }
+}
---
+++ Chapter 13/DemoTLS/Services/SessionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoTLS {
+    public static class SessionService {
+        public static string CurrentAlgorithm { get; set; }
+
+        public static string GenerateSharedKeyWithPrivateKeyAndRandomSessionKey() {
+            return "SHARED_SECRET";
+        }
+    }
+}
