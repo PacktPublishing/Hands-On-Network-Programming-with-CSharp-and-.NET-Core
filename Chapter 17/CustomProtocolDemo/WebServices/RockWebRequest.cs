dd50a76ea114c5f8abe753d286e19e8606b28a8a
BEEMO\shurns <shburns02@gmail.com>
2019-03-10 20:42:50 -0400

    Formatting fix.

--- Chapter 17/CustomProtocolDemo/WebServices/RockWebRequest.cs
+++ Chapter 17/CustomProtocolDemo/WebServices/RockWebRequest.cs
@@ -22,16 +22,16 @@ namespace CustomProtocolDemo {
         public override WebResponse GetResponse() {
             var messageString = ConcatenateRecords();
             var message = Encoding.ASCII.GetBytes(messageString);
-var checksum = SHA256.Create().ComputeHash(message);
+            var checksum = SHA256.Create().ComputeHash(message);
 
-var byteList = new List<byte>();
-byteList.AddRange(GetHeaderBytes(message.Length));
-byteList.AddRange(checksum);
-byteList.AddRange(message);
-TcpClient client = new TcpClient(RequestUri.Host, RequestUri.Port);
-var stream = client.GetStream();
-stream.Write(byteList.ToArray(), 0, byteList.Count);
-return new RockWebResponse(stream);
+            var byteList = new List<byte>();
+            byteList.AddRange(GetHeaderBytes(message.Length));
+            byteList.AddRange(checksum);
+            byteList.AddRange(message);
+            TcpClient client = new TcpClient(RequestUri.Host, RequestUri.Port);
+            var stream = client.GetStream();
+            stream.Write(byteList.ToArray(), 0, byteList.Count);
+            return new RockWebResponse(stream);
         }
 
         private string ConcatenateRecords() {
@@ -45,18 +45,18 @@ return new RockWebResponse(stream);
             return messageBuilder.ToString();
         }
 
-private IEnumerable<byte> GetHeaderBytes(int messageSize) {
-    var headerBytes = new List<byte>();
-    int verbAndSize = (int)Verb | (messageSize >> 2);
-    headerBytes.AddRange(BitConverter.GetBytes(verbAndSize));
+        private IEnumerable<byte> GetHeaderBytes(int messageSize) {
+            var headerBytes = new List<byte>();
+            int verbAndSize = (int)Verb | (messageSize >> 2);
+            headerBytes.AddRange(BitConverter.GetBytes(verbAndSize));
 
-    //Add empty byte padding in the FIELDS header
-    for (var i = 0; i < 20; i++) {
-        headerBytes.Add(0b00000000);
-    }
+            //Add empty byte padding in the FIELDS header
+            for (var i = 0; i < 20; i++) {
+                headerBytes.Add(0b00000000);
+            }
 
-    headerBytes.AddRange(BitConverter.GetBytes(Fields));
-    return headerBytes;
-}
+            headerBytes.AddRange(BitConverter.GetBytes(Fields));
+            return headerBytes;
+        }
     }
 }
