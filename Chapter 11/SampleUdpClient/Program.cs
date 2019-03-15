26202b7a9f54b545f28298c9b6615dc9fe0de99e
BEEMO\shurns <shburns02@gmail.com>
2019-03-15 00:43:33 -0400

    Resolved formatting of SampleUdpClient

--- Chapter 11/SampleUdpClient/Program.cs
+++ Chapter 11/SampleUdpClient/Program.cs
@@ -7,22 +7,20 @@ using System.Threading.Tasks;
 
 namespace SampleUdpClient {
     class Program {
-public static async Task Main(string[] args) {
-  using (var client = new UdpClient(34567)) {
-    var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
+        public static async Task Main(string[] args) {
+            using (var client = new UdpClient(34567)) {
+                var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
 
-    client.Connect(remoteEndpoint);
+                var message = "Testing UDP";
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(messageBytes, messageBytes.Length, remoteEndpoint);
 
-    var message = "Testing UDP";
-    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-    await client.SendAsync(messageBytes, messageBytes.Length);
+                var response = await client.ReceiveAsync();
+                var responseMessage = Encoding.UTF8.GetString(response.Buffer);
+                Console.WriteLine(responseMessage);
 
-    var response = await client.ReceiveAsync();
-    var responseMessage = Encoding.UTF8.GetString(response.Buffer);
-    Console.WriteLine(responseMessage);
-
-    Thread.Sleep(10000);
-  }
-}
+                Thread.Sleep(10000);
+            }
+        }
     }
 }
