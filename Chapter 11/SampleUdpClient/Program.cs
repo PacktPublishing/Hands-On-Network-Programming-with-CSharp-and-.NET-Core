f64090d243f081bede5fa29a4dde075c5859ee8c
BEEMO\shurns <shburns02@gmail.com>
2019-03-15 01:25:06 -0400

    Update to the code base to be consistent with chapter content.

--- Chapter 11/SampleUdpClient/Program.cs
+++ Chapter 11/SampleUdpClient/Program.cs
@@ -11,9 +11,11 @@ namespace SampleUdpClient {
             using (var client = new UdpClient(34567)) {
                 var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
 
+                client.Connect(remoteEndpoint);
+
                 var message = "Testing UDP";
                 byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-                await client.SendAsync(messageBytes, messageBytes.Length, remoteEndpoint);
+                await client.SendAsync(messageBytes, messageBytes.Length);
 
                 var response = await client.ReceiveAsync();
                 var responseMessage = Encoding.UTF8.GetString(response.Buffer);
--- Chapter 11/SampleUdpServer/Program.cs
+++ Chapter 11/SampleUdpServer/Program.cs
@@ -10,9 +10,7 @@ namespace SampleUdpServer {
         public static async Task Main(string[] args) {
             using (var client = new UdpClient(45678)) {
                 IPEndPoint remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 34567);
-
-                client.Connect(remoteEndpoint);
-
+               
                 var incomingMessage = await client.ReceiveAsync();
                 var message = Encoding.UTF8.GetString(incomingMessage.Buffer);
                 Console.WriteLine(message);
@@ -20,7 +18,7 @@ namespace SampleUdpServer {
 
                 var responseString = $"Packet received from client: {message}";
                 byte[] response = Encoding.UTF8.GetBytes(responseString);
-                await client.SendAsync(response, response.Length);
+                await client.SendAsync(response, response.Length, remoteEndpoint);
             }
             Thread.Sleep(10000);
         }
