999bcedf030f01631b84470d29a27e80ed6cb351
BEEMO\shurns <shburns02@gmail.com>
2019-03-17 23:02:46 -0400

    Minor fix

--- Chapter 9/FitnessApp/Services/FitnessDataStoreClient.cs
+++ Chapter 9/FitnessApp/Services/FitnessDataStoreClient.cs
@@ -1,10 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace FitnessApp {
     public interface IDataStoreClient {
